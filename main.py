1b86de01eaf8af3dfd6c931fd3cfa0d3c8aa0508
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-30 18:12:31 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def exponential(lam):
+  # Write your function to generate an exponential random variable here
+  
+lamd = 2
+samples = 151*[0]
+for i in range(151) : 
+  # Write code here to set each element of the list called samples equal to a sample from 
+  # an exponential random variable with parameter lamd
+  
+  
+# Write code to find the median, and the 10th, 25th, 75th and 90th percentiles here.  You need to 
+# set the values of the variables that I have creeated for you here as otherwise the tests will 
+# not pass.
+
+
+median = 
+percentile10 = 
+percentile25 = 
+percentile75 =
+percentile90 = 
+print( percentile10, percentile25, median, percentile75, percentile90 )
